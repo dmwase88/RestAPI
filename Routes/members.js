08013b5b309ac481dce42a8aa8439e3f4608dbbd
Mwase <shop@shops-MacBook-Air.local>
2023-01-09 13:30:50 +0300

    update the API

--- Routes/members.js
+++ Routes/members.js
@@ -1,47 +1,57 @@
 import express from 'express';
-
-//import members from '../controllers/memberscontroller.js'
-
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 const router = express.Router();
-const members = [
-    {
-        "firstName" : "Ethan",
-        "lastName" : "Mwase",
-        "id": 1,
-        "age": 8
-    },
 
-    {
-        "firstName" : "james",
-        "lastName" : "gary",
-        "id": 2,
-        "age": 40
-    },
-
-    {
-        "firstName" : "john",
-        "lastName" : "doe",
-        "id": 3,
-        "age": 20
-    }
-
-]
-
-
+let members = []
 
 router.get('/' , (req,res)=> {
     res.json(members)
 });
 
 router.post('/' , (req,res)=> {
-    const member = {
-        id:members.length + 1,
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        age:req.body.age
- }
- members.push(member)
- res.status('200').send('Member was added successfully')
+    const member = req.body;
+
+    members.push({ ...member, id: uuidv4() });
+ 
+ res.send(`Member with name ${member.firstName} was added succesfully`)
 });
+
+router.delete('/:id' ,(req,res) =>{
+
+    const { id } =req.params;
+    members = members.filter((member)=> member.id !== id);
+    res.send (`User with ${uuidv4() } deleted `);
+});
+
+
+router.get ('/:id', (req,res) => {
+const { id } = req.params;
+const memberid = members.find((member) => member.id === id );
+ res.send(memberid);
+
+})
+
+router.patch ('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName , lastName , age } = req.body;
+    const userpatch = members.find((member) => member.id === id );
+
+if(firstName){
+userpatch.firstName = firstName;
+}
+
+if(age){
+    userpatch.age = age;
+}
+
+if(lastName){
+    userpatch.lastName = lastName;
+}
+res.send(`Customer with ${id} has been updated`);
+
+ })
+
+
 export default router;
 
